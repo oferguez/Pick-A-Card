2a04af203e86775672e714c761dd2d1dc6bc5d7e
oferguez <guez.ofer@gmail.com>
2023-11-04 07:48:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetchCommitInfo();
+});
+
+function fetchCommitInfo() {
+    const repoOwner = "oferguez"; // Replace with your GitHub username
+    const repoName = "Pick-A-Card"; // Replace with your repository name
+
+    fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/commits`)
+        .then(response => response.json())
+        .then(data => {
+            const latestCommit = data[0];
+            const commitHash = latestCommit.sha;
+            const commitDate = new Date(latestCommit.commit.author.date).toLocaleString();
+            
+            document.getElementById("commitInfo").textContent = `Latest Commit: ${commitHash} at ${commitDate}`;
+        })
+        .catch(error => {
+            console.error("Error fetching commit info:", error);
+            document.getElementById("commitInfo").textContent = "Error fetching commit info";
+        });
+}
+
+
+
 const images = [
 'IMG_2023-11-04-072527.jpeg',
 'IMG_2023-11-04-072824.jpeg',
